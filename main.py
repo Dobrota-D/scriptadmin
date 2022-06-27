634ee5dec79e8fa2644853c32effe915b8d68e03
Léo <leo.gregori95@gmail.com>
2022-06-27 14:09:05 +0200

    4 first functions done + Doc

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
 
 def getUserInput():
     """
-
-    :return:
+    Function that ask the User for a string
+    :return: to the separation of each character
     """
-    return true;
+    userInput = input("Entrez des caractères à convertir base64: ")
+    seperateChar(userInput)
 
 def seperateChar(userInput):
     """
-
-    :param userInput:
-    :return:
+    Function that separate all character in an array
+    :param userInput: String from the user
+    :return: to modification of each character to an ascii
     """
-    return true;
+    charList = []
+    for x in userInput:
+        charList.append(x)
+    charToAscii(charList)
 
-def charToAscii(char):
+def charToAscii(charList):
     """
+    Function that modify each character element in the array to an ascii element
+    :param charList: Array of character
+    :return: to modification of each ascii element to a binary element
+    """
+    index = 0
+    while index < len(charList):
+        charList[index] = ord(charList[index])
+        index += 1;
+    asciiToBinary(charList)
 
-    :param char:
-    :return:
+def asciiToBinary(charList):
     """
-    return true;
-
-def asciiToBinary(asciiChar):
+    Function that modify each ascii element in the array to a binary element
+    :param charList: Array of ascii element
+    :return: to modification of each binary element to a binary element on 8 Bits
     """
-
-    :param asciiChar:
-    :return:
-    """
-    return true;
+    index = 0
+    while index < len(charList):
+        charList[index] = str(int(bin(charList[index])[2:]))
+        index += 1;
+    print(charList)
+    return True;
 
 def binaryTo8Bits(binaryChar):
     """
@@ -48,7 +61,7 @@ def groupBinaries(binaryList):
 
 def separateGroupBinariesIn6Bits(binariesString):
     """
-    
+
     :param binariesString:
     :return:
     """
@@ -93,3 +106,5 @@ def completeBase64StringTo8(base64String):
     :return:
     """
     return true;
+
+getUserInput()
