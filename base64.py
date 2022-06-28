62398b53ac631dc0f588a4dd7ca15b92e9cc2f9e
Ugo <ugo.prenat@gmail.com>
2022-06-28 10:58:41 +0200

    base64 encode/decode CLI

---
+++ base64.py
@@ -0,0 +1,54 @@
+import argparse
+
+def get_source(string, filename):
+  """
+  :param string: string from -s arg
+  :apram filename: link to file from -f arg
+  :return: the content to decode/encode
+  """
+  if filename:
+    # If a filename is provided, get his content
+    with open(filename) as f:
+      content = f.read()
+      f.close()
+      return content
+  
+  return string
+
+
+
+parser = argparse.ArgumentParser()
+
+encode_or_decode_group = parser.add_mutually_exclusive_group(required=True)
+encode_or_decode_group.add_argument("-encode", help="Encodage d'une chaîne de caractères ASCII vers base 64", action="store_true")
+encode_or_decode_group.add_argument("-decode", help="Décodage d'une chaîne de caractères Base 64 vers ASCII", action="store_true")
+
+string_or_filename_group = parser.add_mutually_exclusive_group(required=True)
+string_or_filename_group.add_argument("-s", "--string", help="Chaîne de caractères à encoder/décoder", action="store")
+string_or_filename_group.add_argument("-f", "--filename", help="Chemin vers un fichier à encoder/décoder", action="store")
+
+parser.add_argument("-o", "--output", help="Chemin vers le fichier de sortie")
+
+args = parser.parse_args()
+
+
+# get the content to encode or decode from the -s/-f args
+source = get_source(args.string, args.filename)
+
+if args.encode:
+  
+  print(f"Encodage de la chaîne de caractères '{source}'")
+  print(f"Fichier de sortie : {args.output}" if args.output else "Pas de fichier de sortie")
+  
+  # -------------------------------------------
+  # la_fonction_qui_encode(source, args.output)
+  # -------------------------------------------
+  
+elif args.decode:
+  
+  print(f"Décodage de la chaîne de caractères '{source}'")
+  print(f"Fichier de sortie : {args.output}" if args.output else "Pas de fichier de sortie")
+  
+  # -------------------------------------------
+  # la_fonction_qui_decode(source, args.output)
+  # -------------------------------------------
